d593e7e008c200eca2cb0b692c1fbba1a41bf811
anniem401-lab <moralesannie401@gmail.com>
2025-08-31 17:39:59 -0700

    Add files via upload

---
+++ 210-lab-4.cpp
@@ -0,0 +1,23 @@
+// COMSC-210 | Lab 4 | Annie Morales
+// IDE used: Visual Studio Code
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Code to make it output the numbers 1-100
+
+    int i; // cycle counter
+
+    cout << endl;
+    cout << "Numbers 1-100: ";
+
+    for (i = 1; i <= 100; i++)
+        
+        cout << i << " ";
+        cout << endl << endl;
+
+    return 0;    
+}
